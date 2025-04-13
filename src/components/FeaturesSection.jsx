05f5562556fa3b353081776d6b688a669101ba71
SnappiBrawn <creativeswaroop@gmail.com>
2025-04-13 21:43:24 +0530

    update

--- src/components/FeaturesSection.jsx
+++ src/components/FeaturesSection.jsx
@@ -13,7 +13,7 @@ const FeaturesSection = () => {
           <p className="text-2xl px-4 lg:px-0 lg:text-[2.75rem]/16 font-bold leading-tight text-gray-900">
             Real-time, Precise Language Translation – Available around-the-clock, Anywhere, At Your Fingertips
           </p>
-          <p className="text-sm/6 lg:text-base/7 mt-6 text-gray-500 max-w-xl px-4 lg:px-0">
+          <p className="text-sm/6 lg:text-base/7 mt-6 text-gray-500 max-w-xl px-4 lg:px-0 lg:text-left text-justify">
           <b>Alitas AI</b> provides effortless and uninterrupted, instant translation on-the-spot 24/7 to facilitate seamless communication between
             healthcare providers and patients. Fuelled by cutting-edge AI, the platform offers accurate translations of words, sentences, and
             full-fledged conversations—guaranteeing seamless and confident conversations on both ends.
@@ -28,7 +28,7 @@ const FeaturesSection = () => {
         </div>
       </div>
 
-      <div className="flex flex-col lg:flex-row-reverse items-center justify-between gap-10 text-right lg:text-left">
+      <div className="flex flex-col lg:flex-row-reverse items-center justify-between gap-10 text-left lg:text-left">
         <div className="lg:w-1/2">
           <p className="text-(--color-section-heading) lg:text-sm text-xs font-bold uppercase mb-5 px-4 lg:px-0">
             140,000 incidents of misdiagnosis in Australia. 80% attributed to communication issues.
@@ -36,7 +36,7 @@ const FeaturesSection = () => {
           <p className="text-2xl px-4 lg:px-0 lg:text-[2.75rem]/16 font-bold leading-tight text-gray-900">
             Efficient Communication Through Context-Driven Medical Terminology
           </p>
-          <p className="text-sm/6 lg:text-base/7 mt-6 text-gray-500 max-w-xl text-right lg:text-left px-4 lg:px-0">
+          <p className="text-sm/6 lg:text-base/7 mt-6 text-gray-500 max-w-xl px-4 lg:px-0 lg:text-left text-justify">
           Alitas AI enables clear and effective communication, between healthcare providers and patients, owing to a thorough knowledge of medical
             terminology, providing superior-quality and culturally-appropriate care to patients from diverse linguistic backgrounds.
             <br />
@@ -50,7 +50,7 @@ const FeaturesSection = () => {
         </div>
       </div>
 
-      <div className="flex flex-col lg:flex-row items-center justify-between gap-10 text-center lg:text-left">
+      <div className="flex flex-col lg:flex-row items-center justify-between gap-10 text-left lg:text-left">
         <div className="lg:w-1/2">
           <p className="text-(--color-section-heading) lg:text-sm text-xs font-bold uppercase mb-5 px-4 lg:px-0">
             12.9 million individuals may have had their medical history exposed in Australia. Is the patient's data secure?
@@ -58,7 +58,7 @@ const FeaturesSection = () => {
           <p className="text-2xl px-4 lg:px-0 lg:text-[2.75rem]/16 font-bold leading-tight text-gray-900">
             Protecting Every Conversation – Privacy-Driven Communication in Healthcare
           </p>
-          <p className="text-sm/6 lg:text-base/7 mt-6 text-gray-500 max-w-xl text-justify px-4 lg:px-0">
+          <p className="text-sm/6 lg:text-base/7 mt-6 text-gray-500 max-w-xl px-4 lg:px-0 lg:text-left text-justify">
             <b>Alitas AI</b> places security at the forefront, ensuring every communication between healthcare provider and patient remains confidential. By
             removing the need for third-party presence during medical consultations between patients and providers, Alitas enables a seamless and
             secure mode of communication.
